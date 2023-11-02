f1021719484ba6032a1e123602efba4d854e64ec
katelyn-candee <123839927+katelyn-candee@users.noreply.github.com>
2023-11-01 20:18:13 -0400

    Create html outline for reviews CRUD pages
    Drafted basic html requirements for `food-item-reviews.php`, `review-add.php` and `review-update.php`

---
+++ food-item-reviews/food-item-reviews.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Reviews</title>
+	</head>
+	</body>
+		<h1>Food Review App</h1>
+		<h2>Reviews for [food item]</h2>
+		<div>
+			<h3>[Review title]</h3>
+			<div>
+				<p>[Review rating]</p>
+			</div>
+			<div>
+				<p>[Review description]</p>
+				<a href='review-update.php'>Update</a>
+			<a href='food-item-reviews.php'>Delete</a><br>
+			</div>
+			<div>
+				<img src='placeholder-user-photo.gif'></img>
+				<h4>[Review author/user]</h4>
+			</div>
+		</div>
+		<a href='review-add.php'><button>Add a review</button></a>
+	</body>
+</html>
---
+++ food-item-reviews/review-update.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Add Review</title>
+	</head>
+	</body>
+		<h1>Food Review App</h1>
+		<h2>Your review for [food item]</h2>
+		<form method='post' action='food-item-reviews.php'>
+			<div>
+				Rating:
+				<input type ="radio" name='rating' value ='1'>1 star
+				<input type ="radio" name='rating' value ='2'>2 stars
+				<input type ="radio" name='rating' value ='3'>3 stars
+				<input type ="radio" name='rating' value ='4'>4 stars
+				<input type ="radio" name='rating' value ='5'>5 stars
+			</div>
+			<div>
+				Review title:<input type='text'></input><br>
+				Review description:<input type='text'></input><br>
+			</div>
+			<input type='submit' value='Update review'></input>
+		</form>
+	</body>
+</html>
