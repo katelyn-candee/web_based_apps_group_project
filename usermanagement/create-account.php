20527da66cf7df0fdccdd52cd889f73bb082c94c
beegreene <brookeegreene@icloud.com>
2023-11-25 17:09:44 -0800

    Updated Create Account page
    page posts data from form to user and member tables... still finding error with posting to restaurant table

--- usermanagement/create-account.php
+++ usermanagement/create-account.php
@@ -29,9 +29,6 @@
 
                 <label for="last_name">Last Name:</label>
                 <input type="text" name="last_name">
-
-                <label for="email">Email:</label>
-                <input type="email" name="email">
                 
                 <label for="city">City:</label>
                 <input type="text" name="city">
@@ -41,23 +38,33 @@
             </div>
 
             <div id="restaurantFields" class="hidden">
-                <label for="restaurant_name">Restaurant Name:</label>
-                <input type="text" name="restaurant_name">
+                <label for="name">Restaurant Name:</label>
+                <input type="text" name="name">
+                
+                <label for="type">Restaurant Type:</label>
+                <input type="text" name="type">
 
                 <label for="description">Description:</label>
                 <textarea name="description" rows="4"></textarea>
 
                 <label for="address">Address:</label>
                 <input type="text" name="address">
+                
+                <label for="phone">Phone:</label>
+                <input type="tel" name="phone">
+                
+                <label for="website">Website:</label>
+                <input type="url" name="website">
+
+                <label for="email">Email:</label>
+                <input type="email" name="email">
+
+                <label for="image">Firm Image:</label>
+                <input type="file" id="image" name="image" accept="image/*">
 
                 <label for="owner_name">Owner Name:</label>
                 <input type="text" name="owner_name">
 
-                <label for="phone">Phone:</label>
-                <input type="tel" name="phone">
-
-                <label for="email">Email:</label>
-                <input type="email" name="email">
             </div>
 
             <input type="submit" value="CREATE ACCOUNT">
@@ -96,34 +103,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $role = $_POST["role"];
 
     $query = "INSERT INTO user (username, password, role) VALUES ('$username', '$password', '$role')";
-//not posting to member/restaurant table - only user
+//not posting to restaurant table 
     if ($conn->query($query)) {
         $user_id = $conn->insert_id; 
 
         if ($role == "member") {
             $first_name = $_POST["first_name"];
             $last_name = $_POST["last_name"];
-            $email = $_POST["email"];
             $city = $_POST["city"];
             $state = $_POST["state"];
-
-            $query_member = "INSERT INTO member (user_id, first_name, last_name, email, city, state) VALUES ('$user_id', '$first_name', '$last_name', '$email', '$city', '$state')";
+            
+            $query_member = "INSERT INTO member (user_id, first_name, last_name, city, state) VALUES ('$user_id', '$first_name', '$last_name', '$city', '$state')";
             $conn->query($query_member);
         } elseif ($role == "restaurant") {
-            $restaurant_name = $_POST["restaurant_name"];
+            $name = $_POST["name"];
+            $type = $_POST["type"];
             $description = $_POST["description"];
             $address = $_POST["address"];
-            $owner_name = $_POST["owner_name"];
             $phone = $_POST["phone"];
+            $website = $_POST["website"];
             $email = $_POST["email"];
+            $photo = $_POST["photo"];
+            $owner_name = $_POST["owner_name"];
 
-            $query_restaurant = "INSERT INTO restaurant (user_id, restaurant_name, description, address, owner_name, phone, email) VALUES ('$user_id', '$restaurant_name', '$description', '$address', '$owner_name', '$phone', '$email')";
+            $query_restaurant = "INSERT INTO restaurant (user_id, name, type, description, address, phone, website, email, photo, owener_name) VALUES ('$user_id', '$name', '$type', '$description', '$address', '$phone', '$website', '$email', '$photo', '$owner_name')";
             $conn->query($query_restaurant);
         }
 
-        echo "Account created successfully!";
+        header("Location: ../home/home-page.php");
     } else {
-        echo "Error creating account: " . $conn->error;
+        echo "Error creating account: ".$conn->error;
     }
 }
 
