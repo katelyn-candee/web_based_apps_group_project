5157f23da5d402ae9915c38ab574171d90f35f3e
katelyn-candee <123839927+katelyn-candee@users.noreply.github.com>
2023-12-01 19:18:21 -0500

    Update account CRUD ops

--- usermanagement/update-account.php
+++ usermanagement/update-account.php
@@ -63,7 +63,7 @@ if ($member_result->num_rows > 0 && $user_result->num_rows > 0) {
 	First Name: <input type='text' name='first_name' value='$member_row[first_name]'>
 	Last Name: <input type='text' name='last_name' value='$member_row[last_name]'>
 	Username: <input type='text' name='username' value='$user_row[username]'>
-	Password: <input type='text' name='password' value='$user_row[password]'>
+	Password: <input type='text' name='password'>
 	City: <input type='text' name='city' value='$member_row[city]'>
 	State: <input type='text' name='state' value='$member_row[state]'>
 	Account Type: $user_row[role]
@@ -102,7 +102,7 @@ _END;
 	Email: <input type='text' name='email' value='$restaurant_row[email]'>
 	Photo: <input type='text' name='photo' value='$restaurant_row[photo]'>
 	Username: <input type='text' name='username' value='$user_row[username]'>
-	Password: <input type='text' name='password' value='$user_row[password]'>
+	Password: <input type='text' name='password'>
 	Account Type: $user_row[role]
 	
 	</pre>
@@ -124,41 +124,61 @@ _END;
 
 
 if(isset($_POST['update'])){
-	$user_id= mysql_entities_fix_string($conn,$_POST['user_id']);
-	$first_name= mysql_entities_fix_string($conn,$_POST['first_name']);
-	$last_name= mysql_entities_fix_string($conn,$_POST['last_name']);
-	$username= mysql_entities_fix_string($conn,$_POST['username']);
-	$password= mysql_entities_fix_string($conn,$_POST['password']);
-	$token = password_hash($password, PASSWORD_DEFAULT); // Hash the password
-	$city= mysql_entities_fix_string($conn,$_POST['city']);
-	$state= mysql_entities_fix_string($conn,$_POST['state']);
-	$name= mysql_entities_fix_string($conn,$_POST['name']);
-	$owner_name= mysql_entities_fix_string($conn,$_POST['owner_name']);
-	$type= mysql_entities_fix_string($conn,$_POST['type']);
-	$description= mysql_entities_fix_string($conn,$_POST['description']);
-	$address= mysql_entities_fix_string($conn,$_POST['address']);
-	$phone =  mysql_entities_fix_string($conn,$_POST['phone']);
-	$website= mysql_entities_fix_string($conn,$_POST['website']);
-	$user_id= mysql_entities_fix_string($conn,$_POST['user_id']);
-	$email= mysql_entities_fix_string($conn,$_POST['email']);
-	$photo= mysql_entities_fix_string($conn,$_POST['photo']);
 	
-	$u_query = "UPDATE user SET username='$username',password='$token' where user_id=$user_id";
+	if ($member_result->num_rows > 0 && $user_result->num_rows > 0){
+		
+		$user_id= mysql_entities_fix_string($conn,$_POST['user_id']);
+		$first_name= mysql_entities_fix_string($conn,$_POST['first_name']);
+		$last_name= mysql_entities_fix_string($conn,$_POST['last_name']);
+		$username= mysql_entities_fix_string($conn,$_POST['username']);
+		$password= mysql_entities_fix_string($conn,$_POST['password']);
+		$token = password_hash($password, PASSWORD_DEFAULT); // Hash the password
+		$city= mysql_entities_fix_string($conn,$_POST['city']);
+		$state= mysql_entities_fix_string($conn,$_POST['state']);
+		
+		$u_query = "UPDATE user SET username='$username',password='$token' where user_id=$user_id";
 	
-	$u_result = $conn->query($u_query); 
-	if(!$u_result) die($conn->error);
+		$u_result = $conn->query($u_query); 
+		if(!$u_result) die($conn->error);
+		
+		$m_query = "UPDATE member SET first_name='$first_name', last_name='$last_name', city='$city', state='$state' where user_id=$user_id";
+		
+		$m_result = $conn->query($m_query); 
+		if(!$m_result) die($conn->error);
+		
+	} else if ($restaurant_result->num_rows > 0 && $user_result->num_rows > 0) {
+		
+		
+		$user_id= mysql_entities_fix_string($conn,$_POST['user_id']);
+		$username= mysql_entities_fix_string($conn,$_POST['username']);
+		$password= mysql_entities_fix_string($conn,$_POST['password']);
+		$token = password_hash($password, PASSWORD_DEFAULT); // Hash the password
+		$name= mysql_entities_fix_string($conn,$_POST['name']);
+		$owner_name= mysql_entities_fix_string($conn,$_POST['owner_name']);
+		$type= mysql_entities_fix_string($conn,$_POST['type']);
+		$description= mysql_entities_fix_string($conn,$_POST['description']);
+		$address= mysql_entities_fix_string($conn,$_POST['address']);
+		$phone =  mysql_entities_fix_string($conn,$_POST['phone']);
+		$website= mysql_entities_fix_string($conn,$_POST['website']);
+		$user_id= mysql_entities_fix_string($conn,$_POST['user_id']);
+		$email= mysql_entities_fix_string($conn,$_POST['email']);
+		$photo= mysql_entities_fix_string($conn,$_POST['photo']);
+		
+		$u_query = "UPDATE user SET username='$username',password='$token' where user_id=$user_id";
 	
-	$m_query = "UPDATE member SET first_name='$first_name', last_name='$last_name', city='$city', state='$state' where user_id=$user_id";
+		$u_result = $conn->query($u_query); 
+		if(!$u_result) die($conn->error);
+		
+		$r_query = "UPDATE restaurant SET name='$name', owner_name='$owner_name', type='$type', description='$description', address='$address', phone='$phone', website='$website', email='$email',photo='$photo' where user_id=$user_id";
+		
+		$r_result = $conn->query($r_query); 
+		if(!$r_result) die($conn->error);
+		
+	}
 	
-	$m_result = $conn->query($m_query); 
-	if(!$m_result) die($conn->error);
 	
-	$r_query = "UPDATE restaurant SET name='$name', owner_name='$owner_name', type='$type', description='$description', address='$address', phone='$phone', website='$website', email='$email',photo='$photo' where user_id=$user_id";
 	
-	$r_result = $conn->query($r_query); 
-	if(!$r_result) die($conn->error);
-	
-	header("Location: account-details.php");
+	Header("Location: ../usermanagement/account-details.php");
 }
 
 $conn->close();
