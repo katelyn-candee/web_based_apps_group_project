d57867bb2eed480d660ebda83482b5066f9e5302
HJenson <60150236+HJenson@users.noreply.github.com>
2023-12-01 20:37:28 -0700

    update

--- food/food-delete.php
+++ food/food-delete.php
@@ -18,7 +18,13 @@ if(isset($_POST['delete']))
 {
 	$food_item_id = $_POST['food_item_id'];
 
-	$query = "DELETE FROM food_item WHERE food_item_id='$food_item_id' ";
+	$query = "DELETE FROM food_item WHERE food_item_id='$food_item_id'";
+	
+	//Run the query
+	$result = $conn->query($query); 
+	if(!$result) die($conn->error);
+	
+	$query = "DELETE FROM review WHERE food_item_id='$food_item_id'";
 	
 	//Run the query
 	$result = $conn->query($query); 
